c5def729f071fd1d9535ffa6fa8490a746484ca9
Edgardo Garofalo <egarofalo83@gmail.com>
2019-06-21 17:23:40 -0300

    - Mejorando la Experiencia de Usuario durante la peticion

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,41 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./styles/BadgesList.css";
 
 class BadgesList extends React.Component {
-    render() {
-        return (
-            <div className="BadgesList">
-                <ul className="list-unstyled">
-                    {this.props.badges.map(badge => {
-                        return (
-                            <li key={badge.id} className="BadgesListItem">
-                                <img
-                                    src={badge.avatarUrl}
-                                    alt="Avatar"
-                                    className="BadgesListItem__avatar"
-                                />
+	render() {
+		if (this.props.badges.length === 0) {
+			return (
+				<div>
+					<h3>No badges were found</h3>
+					<Link className="btn btn-primary" to="/badges/new">
+						Create new badge
+					</Link>
+				</div>
+			);
+		}
 
-                                <div className="BadgesListItem__info">
-                                    <p className="BadgesListItem__name">
-                                        {badge.firstName} {badge.lastName}
-                                    </p>
+		return (
+			<div className="BadgesList">
+				<ul className="list-unstyled">
+					{this.props.badges.map(badge => {
+						return (
+							<li key={badge.id} className="BadgesListItem">
+								<img
+									src={badge.avatarUrl}
+									alt="Avatar"
+									className="BadgesListItem__avatar"
+								/>
 
-                                    <p className="BadgesListItem__twitter">
-                                        @{badge.twitter}
-                                    </p>
+								<div className="BadgesListItem__info">
+									<p className="BadgesListItem__name">
+										{badge.firstName} {badge.lastName}
+									</p>
 
-                                    <p className="BadgesListItem__jobTitle">
-                                        {badge.jobTitle}
-                                    </p>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
+									<p className="BadgesListItem__twitter">
+										@{badge.twitter}
+									</p>
+
+									<p className="BadgesListItem__jobTitle">
+										{badge.jobTitle}
+									</p>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default BadgesList;
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,107 +5,67 @@ import confLogo from "../images/badge-header.svg";
 import BadgesList from "../components/BadgesList";
 import Link from "react-router-dom/Link";
 
+import api from "../api";
+
 class Badges extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log("1. constructor()");
+	state = {
+		loading: true,
+		error: null,
+		data: undefined
+	};
 
-        this.state = {
-            data: []
-        };
-    }
+	componentDidMount() {
+		this.fetchData();
+	}
 
-    componentDidMount() {
-        console.log("3. componentDidMount()");
+	fetchData = async () => {
+		this.setState({ loading: true, error: null });
 
-        this.timeoutId = setTimeout(() => {
-            this.setState({
-                data: [
-                    {
-                        id: "Badge01",
-                        firstName: "Edgardo",
-                        lastName: "Garofalo",
-                        twitter: "edgardogarofalo",
-                        avatarUrl:
-                            "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=wavatar",
-                        jobTitle: "Desarrollador FullStack",
-                        email: "egarofalo83@gmail.com"
-                    },
-                    {
-                        id: "Badge02",
-                        firstName: "Pablo",
-                        lastName: "Garofalo",
-                        twitter: "pablogarofalo",
-                        avatarUrl:
-                            "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=retro",
-                        jobTitle: "Dr. en economía",
-                        email: "pgarofalo2004@gmail.com"
-                    },
-                    {
-                        id: "Badge03",
-                        firstName: "Federico",
-                        lastName: "Parizi",
-                        twitter: "fparizi",
-                        avatarUrl:
-                            "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=robohash",
-                        jobTitle: "Desarrollador FullStack",
-                        email: "fedeparizi@gmail.com"
-                    }
-                ]
-            });
-        }, 3000);
-    }
+		try {
+			const data = await api.badges.list();
+			this.setState({ loading: false, data: data });
+		} catch (error) {
+			this.setState({ loading: false, error: error });
+		}
+	};
 
-    componentDidUpdate(prevProps, prevState) {
-        console.log("5. componentDidUpdate()");
+	render() {
+		if (this.state.loading === true) {
+			return "Loading...";
+		}
 
-        console.log({
-            prevProps,
-            prevState
-        });
+		if (this.state.error) {
+			return `Error: ${this.state.error.message}`;
+		}
 
-        console.log({
-            props: this.props,
-            state: this.state
-        });
-    }
+		return (
+			<React.Fragment>
+				<div className="Badges">
+					<div className="Badges__hero">
+						<div className="Badges__container">
+							<img
+								className="Badges_conf-logo"
+								src={confLogo}
+								alt="Conf Logo"
+							/>
+						</div>
+					</div>
+				</div>
 
-    componentWillUnmount() {
-        console.log("6. componentWillUnmount()");
-        clearTimeout(this.timeoutId);
-    }
+				<div className="Badges__container">
+					<div className="Badges__buttons">
+						<Link to="/badges/new" className="btn btn-primary">
+							New Badge
+						</Link>
+					</div>
 
-    render() {
-        console.log("2/4. render()");
-
-        return (
-            <React.Fragment>
-                <div className="Badges">
-                    <div className="Badges__hero">
-                        <div className="Badges__container">
-                            <img
-                                className="Badges_conf-logo"
-                                src={confLogo}
-                                alt="Conf Logo"
-                            />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="Badges__container">
-                    <div className="Badges__buttons">
-                        <Link to="/badges/new" className="btn btn-primary">
-                            New Badge
-                        </Link>
-                    </div>
-
-                    <div className="Badges__list">
-                        <BadgesList badges={this.state.data} />
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+					<div className="Badges__list">
+						<BadgesList badges={this.state.data} />
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Badges;
